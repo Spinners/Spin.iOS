7d17c2576055a6c4f79eb1cb6cc01b3f0faa3659
Thibault Wittemberg <thibault.wittemberg@gmail.com>
2019-08-15 20:39:28 -0400

    Initial Commit

---
+++ Spin/Spin.h
@@ -0,0 +1,19 @@
+//
+//  Spin.h
+//  Spin
+//
+//  Created by Thibault Wittemberg on 2019-08-15.
+//  Copyright © 2019 WarpFactor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Spin.
+FOUNDATION_EXPORT double SpinVersionNumber;
+
+//! Project version string for Spin.
+FOUNDATION_EXPORT const unsigned char SpinVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Spin/PublicHeader.h>
+
+
